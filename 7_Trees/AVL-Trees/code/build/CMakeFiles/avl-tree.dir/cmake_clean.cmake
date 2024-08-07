e26864d90c6878217d809bb3aba4647970c1d2ad
Alex <aszabo@uwaterloo.ca>
2024-08-07 03:17:05 +0000

    Cleaning up Trees

---
+++ 7_Trees/AVL-Trees/code/build/CMakeFiles/avl-tree.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/avl-tree.dir/AVL.cpp.o"
+  "CMakeFiles/avl-tree.dir/AVL.cpp.o.d"
+  "avl-tree"
+  "avl-tree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/avl-tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 7_Trees/AVL-Trees/code/build/CMakeFiles/avl-tree.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for avl-tree.
+# This may be replaced when dependencies are built.
